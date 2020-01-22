a9bf06f23bb6753258736e84681d045533970c1a
apriza noer <58714037+aprizanr@users.noreply.github.com>
2020-01-22 08:55:52 +0700

    update_menampilkan responden

--- tabel.php
+++ tabel.php
@@ -146,85 +146,105 @@ $sessionKey= $myJSONRPCClient->get_session_key( LS_USER, LS_PASSWORD );
 					</tr>
 				</tbody>
 			</table>
-	
+
+		<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+			Tampilkan Grafik
+			</button>
+
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			<div class="modal-dialog modal-xl modal-dialog-centered" role="document">
+				<div class="modal-content">
+				<div class="modal-header">
+					<h5 class="modal-title" id="exampleModalCenterTitle">Grafik Biro PKHP</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					<span aria-hidden="true">&times;</span>
+					</button>
+				</div>
+				<div class="modal-body">
+					<div class="chart-container" style="position: relative; height:40vh; width:80vw">
+						<canvas id="chart"  ></canvas>
+					</div>
+
+					<script>
+						let myChart = document.getElementById('chart').getContext('2d');
+
+						// Global Options
+						// Chart.defaults.global.defaultFontFamily = 'Lato';
+						Chart.defaults.global.defaultFontSize = 12;
+						Chart.defaults.global.defaultFontColor = '#777';
+
+						let massPopChart = new Chart(myChart, {
+						type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
+						data:{
+							labels:["Sangat Puas", "Puas", "Cukup", "Kurang Puas", "Tidak Puas"],
+							datasets:[{
+							label:'Poin',
+							data:
+								[
+									<?= $jumlahA1 ?>,
+									<?= $jumlahA2 ?>,
+									<?= $jumlahA3 ?>,
+									<?= $jumlahA4 ?>,
+									<?= $jumlahA5 ?>
+								]
+							,
+							//  backgroundColor:'green',
+							backgroundColor:[
+								'rgba(255, 99, 132, 0.6)',
+								'rgba(54, 162, 235, 0.6)',
+								'rgba(255, 206, 86, 0.6)',
+								'rgba(75, 192, 192, 0.6)',
+								'rgba(153, 102, 255, 0.6)',
+								'rgba(255, 159, 64, 0.6)',
+								'rgba(255, 99, 132, 0.6)'
+							],
+							borderWidth:1,
+							borderColor:'#777',
+							hoverBorderWidth:3,
+							hoverBorderColor:'#000'
+							}]
+						},
+						options:{
+							title:{
+							display:true,
+							text:'Biro PKHP',
+							fontSize:14
+							},
+							legend:{
+							display:true,
+							position:'bottom',
+							labels:{
+								fontColor:'#000'
+							}
+							},
+							layout:{
+							padding:{
+								left:0,
+								right:0,
+								bottom:0,
+								top:0
+							}
+							},
+							tooltips:{
+							enabled:true
+							},
+							maintainAspectRatio: false
+						}
+						});
+					</script>
+
+				</div>
+				<div class="modal-footer">
+					<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+					<button type="button" class="btn btn-primary">Save changes</button>
+				</div>
+				</div>
+			</div>
+			</div>
+
 	</div>
-
-	<div class="chart-container" style="position: relative; height:40vh; width:80vw">
-		<canvas id="chart"  ></canvas>
-	</div>
-
-	<script>
-    let myChart = document.getElementById('chart').getContext('2d');
-
-    // Global Options
-    // Chart.defaults.global.defaultFontFamily = 'Lato';
-     Chart.defaults.global.defaultFontSize = 12;
-     Chart.defaults.global.defaultFontColor = '#777';
-	//  chart.canvas.parentNode.style.height = '128px';
-	// chart.canvas.parentNode.style.width = '128px';
-
-    let massPopChart = new Chart(myChart, {
-      type:'doughnut', // bar, horizontalBar, pie, line, doughnut, radar, polarArea
-      data:{
-        labels:["Sangat Puas", "Puas", "Cukup", "Kurang Puas", "Tidak Puas"],
-        datasets:[{
-          label:'Poin',
-          data:
-			[
-				<?= $jumlahA1 ?>,
-				<?= $jumlahA2 ?>,
-				<?= $jumlahA3 ?>,
-				<?= $jumlahA4 ?>,
-				<?= $jumlahA5 ?>
-			]
-          ,
-        //  backgroundColor:'green',
-           backgroundColor:[
-             'rgba(255, 99, 132, 0.6)',
-             'rgba(54, 162, 235, 0.6)',
-             'rgba(255, 206, 86, 0.6)',
-             'rgba(75, 192, 192, 0.6)',
-             'rgba(153, 102, 255, 0.6)',
-             'rgba(255, 159, 64, 0.6)',
-             'rgba(255, 99, 132, 0.6)'
-           ],
-          borderWidth:1,
-          borderColor:'#777',
-          hoverBorderWidth:3,
-          hoverBorderColor:'#000'
-        }]
-      },
-      options:{
-        title:{
-          display:true,
-          text:'Grafik',
-          fontSize:14
-        },
-        legend:{
-          display:true,
-          position:'bottom',
-          labels:{
-            fontColor:'#000'
-          }
-        },
-        layout:{
-          padding:{
-            left:0,
-            right:0,
-            bottom:20,
-            top:10
-          }
-        },
-        tooltips:{
-          enabled:true
-        },
-		maintainAspectRatio: false
-      }
-    });
-  </script>
-
-
-
 
     <!-- Optional JavaScript -->
     <!-- jQuery first, then Popper.js, then Bootstrap JS -->
